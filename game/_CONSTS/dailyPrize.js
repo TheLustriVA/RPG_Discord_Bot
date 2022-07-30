d9fd1970f72d1ea5e64ea6783ac738111c5458c6
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-07-30 07:49:14 -0400

    Added more to-do's, updated daily prize.

--- game/_CONSTS/dailyPrize.js
+++ game/_CONSTS/dailyPrize.js
@@ -1,22 +1,22 @@
 const dailyPrizes = {
 	0: {
-		gold: 50,
+		gold: 10,
 	},
 	1: {
-		gold: 100,
+		gold: 50,
 		["oak wood"]: 15,
 	},
 	2: {
-		gold: 200,
+		gold: 100,
 		["oak wood"]: 25,
 	},
 	3: {
-		gold: 280,
+		gold: 300,
 		["oak wood"]: 20,
 		["copper ore"]: 20,
 	},
 	4: {
-		gold: 350,
+		gold: 1000,
 		["oak wood"]: 25,
 		["copper ore"]: 25,
 		["yew wood"]: 10,
